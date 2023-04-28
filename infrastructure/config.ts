f9b79703e6fece14cedcefe5685dfad70e25d0f6
Yusuf Birader <yusuf.birader@hotmail.com>
2023-04-28 12:47:03 +0100

    add config envs

---
+++ infrastructure/config.ts
@@ -0,0 +1,4 @@
+export const VERSION = process.env.VERSION || "latest";
+export const CLUSTER_NAME = "symphony-test";
+export const PRIMARY_REGION = "europe-west2"
+export const SECONDARY_REGION = "us-east1"
