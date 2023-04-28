218efcb248418d2ccf9ebac0f72d72c599672e71
Yusuf Birader <yusuf.birader@hotmail.com>
2023-04-28 13:16:22 +0100

    feat: add k8s namespaces

--- infrastructure/main.ts
+++ infrastructure/main.ts
@@ -5,6 +5,7 @@ import { CLUSTER_NAME, PRIMARY_REGION, SECONDARY_REGION } from "./config";
 import { ProjectService } from "@cdktf/provider-google/lib/project-service";
 import { ArtifactRegistryRepository } from "@cdktf/provider-google/lib/artifact-registry-repository";
 import { KubernetesCluster } from "./constructs/kubernetes_cluster";
+import { NamespaceV1 } from "@cdktf/provider-kubernetes/lib/namespace-v1";
 
 class SymphonyInfrastructure extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -57,6 +58,51 @@ class SymphonyInfrastructure extends TerraformStack {
       region: "europe-west2",
       project: projectId.stringValue,
     });
+  }
+}
+
+class SymphonyApplication extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // get project id again since cross-stack reference not supported
+    // get domain name for ingress
+
+    const projectId = new TerraformVariable(this, "projectId", {
+      type: "string",
+      description: "globally unique project id",
+      default: "fourth-elixir-380718",
+    });
+
+    const domainName = new TerraformVariable(this, "domainName", {
+      type: "string",
+      description: "host domain name",
+      default: "symphony.yusufbirader.com",
+    });
+
+    // create provider
+
+    new GoogleProvider(this, "google", {
+      zone: PRIMARY_REGION,
+      project: projectId.stringValue,
+    });
+
+    // access cluster
+
+    const cluster = KubernetesCluster.onCluster(this, CLUSTER_NAME);
+
+    // create namespaces
+
+    const serverNs = new NamespaceV1(this, "server-ns", {
+      metadata: { name: "server" },
+    });
+    const roomsNs = new NamespaceV1(this, "rooms-ns", {
+      metadata: { name: "rooms" },
+    });
+    const dashboardNs = new NamespaceV1(this, "dashboard-ns", {
+      metadata: { name: "dashboard" },
+    });
+
   }
 }
 
